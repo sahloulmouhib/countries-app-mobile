885e2203b77190036b54f5f5ada302c0cde9b0a6
Mouhib Sahloul <>
2023-03-15 17:55:20 +0100

    add link to create ios and android icons

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import Layout from './Layout';
 
 const App = () => {
+  //TODO: change app icon with background color
+  // guide: https://aboutreact.com/react-native-change-app-icon/
+  // icon generator: iconKitchen
   return <Layout />;
 };
 
