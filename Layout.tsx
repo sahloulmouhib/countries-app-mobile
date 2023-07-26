f7f25687f07a394cf322b4b61150716f02cdd82b
Mouhib Sahloul <sahloulmouhib@hotmail.com>
2023-07-26 14:31:41 +0100

    #bootsplash: add splash screen for ios and android

--- Layout.tsx
+++ Layout.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import NavigationContainer from '_navigation/NavigationContainer';
 
 const Layout = () => {
+  const initAndHideSplashScreen = async () => {
+    try {
+      await RNBootSplash.hide({ fade: true });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      initAndHideSplashScreen();
+    }, 1000);
+  }, []);
   return (
     <GestureHandlerRootView style={styles.container}>
       <SafeAreaProvider>
