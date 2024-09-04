90e78f945d4b68a9324a9f62f0434cc33c616c54
Mouhib Sahloul <sahloulmouhib@hotmail.com>
2024-09-04 11:42:14 +0100

    #upgrade-react-native-version: fix eslint errors deleting yarn.lock file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,13 @@ module.exports = {
   root: true,
   extends: ['@react-native', 'prettier'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest'],
   rules: {
     'react-hooks/exhaustive-deps': 'off',
     'lines-between-class-members': [
       'error',
       'always',
       { exceptAfterSingleLine: true },
-    ],
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
     ],
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
